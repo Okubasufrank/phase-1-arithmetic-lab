600aa3dc878bb687dc1af9ebb6288cb605bf3a54
Okubasufrank <franklineokubasu51@gmail.com>
2022-06-23 22:40:26 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
+var num1;
+var num2;
 // Write your code here
+var multiply =(num1, num2) => {
+    return num1 * num2;
+};
+    console.log(multiply(31, 2));
+
+    var random = Math.floor(Math.random() * 10) + 1 ;
+    var max = Math.max(1, 2, 3, 4, 5, 20);
